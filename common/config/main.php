7d3cf52e06e47f05d09ec905c0e648dd55741e1a
Xzibit <kostirko3@mail.ru>
2020-04-13 21:56:12 +0700

    Initialization project

---
+++ common/config/main.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=localhost;dbname=api',
+            'username' => '',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
